683227275a7719431c0797f24fc078f3407a3b83
nkumar007 <naveen.mbuff98@gmail.com>
2023-01-19 17:09:10 +0530

    writing better css with tailwind

---
+++ src/pages/Login.js
@@ -0,0 +1,51 @@
+import { useNavigate } from "react-router-dom";
+
+const details = {
+  username: "nkwinner",
+  email: "nkg@google.com",
+};
+const Login = () => {
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+    navigate();
+  };
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex justify-center items-center w-[100vw] h-[100vh] bg-red-200"
+    >
+      <div className="flex flex-col items-center justify-center gap-2 w-[450px] h-[40%]  bg-blue-400 p-4 rounded-lg">
+        <span className="text-center text-white font-medium text-3xl">
+          Welcome to FoodVilla
+        </span>
+        <label>
+          <input
+            type="text"
+            name="username"
+            placeholder="username"
+            className="p-2 m-2 outline outline-none text-xl rounded-md"
+          />
+        </label>
+        <label>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="email"
+            className="p-2 m-2 outline outline-none text-xl rounded-md"
+          />
+        </label>
+        <button
+          type="submit"
+          className="p-2 text-lg text-white font-medium bg-blue-800 rounded-md"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Login;
