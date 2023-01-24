317be9ec93c8cbc6110e75dd83281736234a0bcf
nkumar007 <naveen.mbuff98@gmail.com>
2023-01-24 19:38:13 +0530

    implementing useFetch custom hook

---
+++ src/components/utils/useFetch.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import { FETCH_RESTAURANT_LIST_URL } from "../../Contants";
+
+const useFetch = () => {
+  const [allRestaurants, setAllRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  useEffect(() => {
+    fetcher();
+  }, []);
+
+  async function fetcher() {
+    const res = await fetch(FETCH_RESTAURANT_LIST_URL);
+    const json = await res.json();
+
+    setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+    setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+  }
+
+  return { allRestaurants, filteredRestaurants, setFilteredRestaurants };
+};
+
+export default useFetch;
